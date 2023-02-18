97c72cb1db59b8bee628de2f70cf92fbb4e1fbfc
Abel Casas <acasasgarcia1@cifpfbmoll.eu>
2023-02-18 18:32:55 +0100

    build(maven/gitignore): create gitignore and maven project

---
+++ fastbreaktruck/src/main/java/edu/craptocraft/fastbreaktruck/App.java
@@ -0,0 +1,13 @@
+package edu.craptocraft.fastbreaktruck;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
